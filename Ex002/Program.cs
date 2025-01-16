b5b8b8b8e267e0e28cd53ab03ea86397bcaaec7a
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-16 19:37:03 -0300

    Adicionar arquivos de projeto.

---
+++ Ex002/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2;
+            float num = 2.35f;
+            char letra = 'c';
+            string nome = "Sarah";
+
+            Console.WriteLine("O número é {0}.",numero);
+            Console.WriteLine("0 valor é R$ {0}.",num);  
+            Console.WriteLine("A letra é {0}.",letra);
+            Console.WriteLine("O nome é {0}.",nome);
+
+            Console.WriteLine("O número é {0}, O valor é R$ {1}, A letra é ({2}) e O meu nome é {3}.", numero, num, letra, nome);
+        }
+    }
+}
---
+++ Ex005/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Digite a nota:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a outra nota:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            num3 = num1 + num2;
+            Console.WriteLine("A somas das duas notas é:{0} + {1}
+        }
+    }
+}
