54719b122708201816de703daf778f7683ab3243
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-23 19:34:08 -0300

    Regressiva para o Ano Novo

---
+++ Ex011/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Casa, Salário, PrestaçãoMensal;
+            int AnosEmpréstimo, MesesEmprestimo;
+
+            Console.Write("Digite o valor da casa: ");
+            Casa = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor do seu salário: ");
+            Salário = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor do seu salário: ");
+            AnosEmpréstimo = Convert.ToInt32(Console.ReadLine());
+
+            MesesEmprestimo = AnosEmpréstimo * 12;
+
+            PrestaçãoMensal = Casa / MesesEmprestimo;
+
+            if (PrestaçãoMensal < Salário * 0.30)
+            {
+                Console.WriteLine("Empréstimo aceito.");
+                Console.WriteLine("Parcelas: {0}", MesesEmprestimo);
+                Console.WriteLine("Valor: R${0} Mensais", PrestaçãoMensal);
+            }
+            else
+            {
+                Console.WriteLine("Empréstimo negado.");
+                Console.WriteLine("O valor da Prestação ultrapassa 30% do seu salário.");
+            }
+                
+            
+            
+        }
+    }
+}
---
+++ Ex012/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          for(int i = 10; i >= 0; i--) 
+          {
+               Console.WriteLine(i);
+          }
+            Console.WriteLine("Feliz Ano Novo.");
+        }     
+    }
+}
