e51df2e011ffd93c2801b1e6be75b489ece696c9
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-23 21:15:08 -0300

    Tabuada.

---
+++ Ex015/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int número, resultado;
+
+            Console.Write("Digite o número para saber o resultado da tabuada: ");
+            número = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = número * i;
+                Console.WriteLine("{0} x {1} = {2}", número, i, resultado);
+            }
+
+
+
+
+        }
+    }
+}
