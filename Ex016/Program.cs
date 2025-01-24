f0f1b8290931e036c8aa0d1c0466d98af0789fb2
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-23 21:40:06 -0300

    Gênero

---
+++ Ex016/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Genero;
+
+            Console.Write("Digite o Gênero: ");
+            Genero = Console.ReadLine().ToUpper();
+
+            while (Genero != "F" && Genero != "M")
+            {
+                Console.Write("Digite o Genêro: ");
+                Genero += Console.ReadLine();
+            }
+            Console.WriteLine("Bem-vindos e bem-vindas ao curso de C#!");
+        }
+    }
+}
