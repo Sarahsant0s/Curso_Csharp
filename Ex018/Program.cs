d597c9262b4f288395bba295597f974242b164dc
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-28 18:55:51 -0300

    Do While

---
+++ Ex018/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = 5;
+            /*
+            while (num < 5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }
+            */
+
+            do
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }while (num < 5);
+        }
+    }
+}
