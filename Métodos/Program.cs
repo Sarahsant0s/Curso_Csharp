3ea53a4c14851a259b4e959ea9785a4af2780fd5
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-30 20:54:59 -0300

    Saudação

---
+++ Métodos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao("Sarah"); // Olá Sarah, seja bem-vinda!
+            soma(10, 20);
+        }
+        // Método Saudação
+        static void Saudacao(string nome)
+        {
+            //Exibe uma mensagem de saudação
+            Console.WriteLine($"Olá {nome}, Seja bem-vinda!");// Olá Sarah, seja bem-vinda!
+        }
+
+        static void soma (int a, int b)
+        {
+        int resultado = a + b;
+        Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+        Console.WriteLine("A soma de {0} + {1} é igual a {2}.", a, b, resultado);
+        Console.WriteLine("A soma de " + a + " + " + b +" é igual a " + resultado + ".");
+        }
+    }
+}
