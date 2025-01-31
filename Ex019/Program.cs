ed5c31585e4d03cfb1039e42074777a82a3bf73f
Sarah <sarah.scosta3@senacsp.edu.br>
2025-01-30 21:51:07 -0300

    Escrever

---
+++ Ex019/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("                   *Calcule a área do triângulo*");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a base do triânguo: ");
+            double Largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o comprimento do tereno (M): ");
+            double Comprimento = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaTerreno(Largura, Comprimento);
+        }
+        static void AreaTerreno(double l, double c)
+        {
+            double a = l * c;
+            Console.WriteLine($"A área do triângulo de {l} x {c} é de {a:F2}.");
+        }
+    }
+    }
+}
